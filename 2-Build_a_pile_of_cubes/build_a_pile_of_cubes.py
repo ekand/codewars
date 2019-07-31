6b2baf2d9379412624c6d92b14ea49de1870bc0b
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-07-31 08:43:10 -0500

    on second attempt it works correctly

--- 2-Build_a_pile_of_cubes/build_a_pile_of_cubes.py
+++ 2-Build_a_pile_of_cubes/build_a_pile_of_cubes.py
@@ -4,24 +4,32 @@ Created on Wed Jul 31 07:58:27 2019
 
 @author: user2
 """
-
-# this is my first attempt. didn't go so well. It took
-# a long time and got the wrong answer
+# on the second attempt, using some math tricks, it works!
 
 
 def find_nb(m):
-    vol = 0 # initialize a running count of the volume
-    n = 1 # initialize count of number of cubes
-    while vol < m:
-        vol = vol + n^3
+    # takes a volume m and returns the number of 
+    # cubes n needed to construct that volume
+    
+    # define a function to calculate (sum(n^3) n = 1 ... x) 
+    # which, via wolfram alpha, = (1 / 4) * (x ^ 2) * (x + 1) ^ 2
+    def sum(x):
+        return (x * x) * (x + 1) * (x + 1) / 4
+    
+    # loop over guesses for n, the number of cubes
+    # until we are <= the target volume
+    n = 0
+    type(sum(n))
+    type(m)
+    while sum(n) < m:
         n += 1
-        print(vol)
-    if vol == m:
-        return n
+
+    # check if we got the target volume
+    if sum(n) == m:
+        return n 
     else:
         return -1
-        
+
 print(find_nb(4183059834009))
 #should be 2022
-# actually gave -1 and took a long time
 
