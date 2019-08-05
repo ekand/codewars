5b3a67811aee27e0a6275bcb79f9ca03e767e5e8
Erik <erik.kristofer.anderson@gmail.com>
2019-08-05 09:01:42 -0500

    added comments to highest scoring word

--- 3-highest_scoring_word/highest_scoring_word.py
+++ 3-highest_scoring_word/highest_scoring_word.py
@@ -1,22 +1,32 @@
 def high(x):
-    # Code here
-    def letter_score(letter):
-        return ord(letter) - 96
+    """ Given a string of words, return the position of the
+    word with the highest score (a worth 1, b worth 2
+    c worth 3, etc...)
 
+    x: string of (lowercase) words separated by spaces
+    """
+    #calcaulate the score for a word
     def word_score(word):
+        '''Calcualates the score of a word
+        word: a lowercase string
+        '''
         score = 0
         for character in word:
-            score += letter_score(character)
+            score += ord(character) - 96
         return score
 
+    # put words into a list
     words = x.split()
+    #initialize variables
     max_score = 0
     i = 0
     index = 0
+    #compare score of words
     for word in words:
         score = word_score(word)
         if score > max_score:
             max_score = score
             index = i
         i += 1
+    #return index of word with highest score     
     return words[index]
