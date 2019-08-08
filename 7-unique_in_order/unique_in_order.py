07c6e3c1593fdff934b7ad41e99128d00b2f855c
Erik <erik.kristofer.anderson@gmail.com>
2019-08-08 06:58:44 -0500

    Solved 7-unique in order

--- 7-unique_in_order/unique_in_order.py
+++ 7-unique_in_order/unique_in_order.py
@@ -2,17 +2,22 @@
 # https://www.codewars.com/kata/unique-in-order
 
 def unique_in_order(iterable):
+    # need to convert str to list
     if type(iterable) == str:
         iterable_string = iterable
         iterable = []
         for character in iterable_string:
             iterable.append(character)
 
-    while len(iterable) > len(set(iterable)):
+    # remove adjacent duplicates until there are none left
+    test = False
+    while test == False:
+        test = True
         for i, item in enumerate(iterable[:-1]):
-            print('i', i, 'item', item, 'iterable', iterable)
+            # look for cases of repeated letters next to one another
             if iterable[i+1] == item:
                 del iterable[i+1]
+                test = False
                 break
-#
+
     return iterable
