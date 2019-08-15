8b16e4159411f77973085e03785ec92bfb363d22
Erik <erik.kristofer.anderson@gmail.com>
2019-08-15 15:45:49 -0500

    added max subarray sum best practice version

--- 9-Maximum_subarray_sum/best_practice_solution.py
+++ 9-Maximum_subarray_sum/best_practice_solution.py
@@ -9,22 +9,20 @@ def maxSequence(arr):
     print('max:', max)
     print('curr:', curr)
     print('')
-    print('for x in arr:')
+    print('`for x in arr:`')
     for x in arr:
         print('x:', x)
         print('curr:', curr)
         curr+=x
-        print('curr+=x, curr:', curr)
+        print('`curr+=x`, curr:', curr)
         if curr<0:curr=0
-        print('if curr<0:curr=0, curr:', curr)
+        print('`if curr<0:curr=0`, curr:', curr)
         if curr>max:max=curr
-        print('if curr>max:max=curr, max:', max)
+        print('`if curr>max:max=curr`, max:', max)
         print('')
-    print('return max, max:', max)
+    print('`return max`, max:', max)
     print('')
     return max
 
 
 print(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])) #should = 6
-
-print(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 6]))
