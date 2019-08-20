43fc721edebf5ac5def2468c824afaec800bda21
Erik <erik.kristofer.anderson@gmail.com>
2019-08-20 09:21:25 -0500

    added comments to unique in order

--- 2019-08-20_unique_in_order/unique_in_order_2.py
+++ 2019-08-20_unique_in_order/unique_in_order_2.py
@@ -1,3 +1,5 @@
+# from kata here: https://www.codewars.com/kata/54e6533c92449cc251001667/solutions/python
+
 def unique_in_order(iterable):
     unique_list = []
     for i, character in enumerate(iterable):
@@ -8,3 +10,8 @@ def unique_in_order(iterable):
                 unique_list.append(character)
         prev = character
     return unique_list
+
+
+# solution voted best practices initialized prev before the for
+# loop, so it was able to dispense with the enumerate and
+# the if else
