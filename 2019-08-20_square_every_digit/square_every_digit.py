9d3121e8ae63ec1d81c94e92ca609431615721a7
Erik <erik.kristofer.anderson@gmail.com>
2019-08-20 09:08:08 -0500

    refactored square every digit

--- 2019-08-20_square_every_digit/square_every_digit.py
+++ 2019-08-20_square_every_digit/square_every_digit.py
@@ -1,7 +1,5 @@
 def square_digits(num):
-    string = str(num)
-    new_string = ""
-    for character in string:
-        new_string += str(int(character)**2)
-
-    return int(new_string)
+    s = ''
+    for character in str(num):
+        s += str(int(character) ** 2)
+    return int(s)
